78df13f60a649d8a5c82cf49c1283510131f3268
nathanstrahs <135379779+nathanstrahs@users.noreply.github.com>
2024-06-28 10:39:51 -0400

    document runMega_experiments.sh

--- run/runMega_experiments.sh
+++ run/runMega_experiments.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Author: Nathan Strahs <nstrahs@bu.edu>
+# Date: Jun 2024
+#
+# USAGE: ./runMega_experiments.sh PARAM_FILE.txt
+#
+# PARAMFILE.txt is to be used in combination with megaSetCreator.py
+# PARAMFILE.txt contains all the necessary filenames for the syncs to be conducted
+
+
 # Check if the correct number of arguments are provided
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 paramFiles.txt"
@@ -14,30 +23,29 @@ if [ ! -f "$paramFile" ]; then
   exit 1
 fi
 
-
+#create a list of syncs to be conducted from the paramFile
 mapfile -t lines < "$paramFile"
 
 for line in "${lines[@]}"; do
 (
+# Read the file line by line and extract filenames
   echo $line
   read -r serverFile clientFile csvFileName <<< "$line"
 
-  # Print the variables for debugging purposes and set as enviornment variables
-  echo "Server File: $serverFile"
-  echo "Client File: $clientFile"
-  echo "CSV File Name: $csvFileName"
-
+  #create a file to be sent to the shell running the experiments
   OUTPUT_FILE="server_client_names.txt"
   echo "${serverFile} ${clientFile} ${csvFileName}" > ${OUTPUT_FILE}
 
   export CSV_FILENAME=$csvFileName
   export CLIENT_FILENAME=$clientFile
   export SERVER_FILENAME=$serverFile
+
+  #run the experiments on the remote
   echo "Running run_experiments.sh"
   ./run_experiments.sh -r "128.197.128.230:/home/nathanstrahs/EXPERIMENTS/MEGA-EXPERIMENTS/$CSV_FILENAME" &
   first_script_pid=$!
 
-  # Set a timeout in seconds
+  # Set a timeout
   timeout=700
   while [ $timeout -gt 0 ]; do
     sleep 1
@@ -54,12 +62,11 @@ for line in "${lines[@]}"; do
     wait $first_script_pid 2>/dev/null
   fi
 
+  #pull the csv of the experiments
   ./run_experiments.sh -p "128.197.128.230:/home/nathanstrahs/EXPERIMENTS/MEGA-EXPERIMENTS/$CSV_FILENAME"
   sleep 2
 
   rm $OUTPUT_FILE
-  echo "Pulled the CSV, moving to next rendition"
+  echo "Pulled the CSV, moving to next params"
   )
 done < "$paramFile"
-
-                                                                                                    62,3          All
